e1774cdc971dba937b9010466be8258e2cd7d092
connor-k <connor-k@users.noreply.github.com>
2013-07-01 21:29:45 -0700

    Create line_encoded_music.py

---
+++ line_encoded_music.py
@@ -0,0 +1,70 @@
+from Myro import *
+init("/dev/tty.Fluke2-0216-Fluke2")
+
+# The notes I'll be using to play Mary Had a Little Lamb
+G4 = 783.99
+A4 = 880
+B4 = 987.77
+
+# Drive slowly across the page with music lines
+motors(-.05,-.05)
+# To stop the Scribbler, wave your hand/something in front of the fluke
+while getObstacle('center') < 6300:
+    left, right = getLine()
+
+    # Beep a different tone depending on what code is present (left, right, both)
+    if left == 1 and right == 1:
+        beep(0.32,G4)
+    elif right == 1:
+        beep(0.32,B4)
+    elif left == 1:
+        beep(0.32,A4)
+    # Don't make any noise if Scribbler is off the track
+
+# When it's done, stop Scribbler
+stop()
+
+
+# If you just want to play Mary had a little lamb without driving, uncomment the
+# song below and comment out the code above (except from and init, of course)
+"""beep(.5, B4)
+beep(.5, A4)
+beep(.5, G4)
+beep(.5, A4)
+
+beep(.5, B4)
+wait(.05)
+beep(.5, B4)
+wait(.05)
+beep(1, B4)
+
+beep(.5, A4)
+wait(.05)
+beep(.5, A4)
+wait(.05)
+beep(1, A4)
+
+beep(.5, B4)
+wait(.05)
+beep(.5, B4)
+wait(.05)
+beep(1, B4)
+wait(.05)
+
+beep(.5, B4)
+beep(.5, A4)
+beep(.5, G4)
+beep(.5, A4)
+
+beep(.5, B4)
+wait(.05)
+beep(.5, B4)
+wait(.05)
+beep(1, B4)
+
+beep(.5, A4)
+wait(.05)
+beep(.5, A4)
+beep(.5, B4)
+beep(.5, A4)
+beep(1, G4)"""
