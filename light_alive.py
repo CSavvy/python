b0460b494b3334871da46987dbd89b30d2f3432e
Kyle <wong.kyle5@gmail.com>
2013-07-01 21:21:07 -0700

    Create light_alive.py

---
+++ light_alive.py
@@ -0,0 +1,25 @@
+from Myro import *
+init("com3")
+Ambient = getLight("center")
+def normalize(v):
+    if v > Ambient:
+        v = Ambient
+    return 1.0 - v/Ambient
+
+def main():
+    while True:
+        C = getLight("center")
+        backward(normalize(C))
+        L = getLight("left")
+        R = getLight("right")
+        print(getLight())
+        turn = normalize(L) - normalize(R)
+        scale = 4
+        print(turn*90*scale)
+        if abs(turn*90*scale) < 90:
+            rotate(turn*90*scale)
+        else:
+            rotate(turn*90)
+            print("scale")
+        wait(.1)
+main()
