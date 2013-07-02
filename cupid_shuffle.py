4607ed27a4c72fd3877a2c75b5d4f902826007f6
Kyle <wong.kyle5@gmail.com>
2013-07-01 21:21:35 -0700

    Create cupid_shuffle.py

---
+++ cupid_shuffle.py
@@ -0,0 +1,29 @@
+def cupid():
+    steptime = .4
+    kick_angle = -35
+
+    for i in range(4):
+        forward(1, steptime)
+        wait(steptime)
+    for i in range(4):
+        backward(1, steptime)
+        wait(steptime)
+    for i in range(2):
+        turnBy(kick_angle)
+        wait(.2)
+        turnBy(-kick_angle)
+        wait(.2)
+        kick_angle = -kick_angle
+    wait(.5)
+    turnBy(-90)
+
+from Myro import *
+init("com7")
+
+s = makeSound("shuffle2.wav")
+for j in range(1): #set number of loops here
+    c = s.Play(0, 32000)
+    wait(5)
+    cupid()
+    wait(.5)
+    cupid()
